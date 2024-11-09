1c63a123ed3583f291cbd3c4baa37086518e1577
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-11-09 14:56:25 +0900

    코드 정리

--- src/main/java/org/example/springsecuritybackend/domain/user/entity/User.java
+++ src/main/java/org/example/springsecuritybackend/domain/user/entity/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.springsecuritybackend.domain.user.dto.request.JoinRequestDto;
 
 import static lombok.AccessLevel.PROTECTED;
 
