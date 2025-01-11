72a42e98d9ecdcb85a7b3ffc64847123fbc5726a
kimregular <regular.jk.kim@gmail.com>
2025-01-11 20:44:17 +0900

    form 회원인증 시스템

--- src/main/java/org/example/springsecuritybackend/domain/user/dto/request/JoinRequestDto.java
+++
@@ -1,4 +0,0 @@
-package org.example.springsecuritybackend.domain.user.dto.request;
-
-public record JoinRequestDto(String username, String password) {
-}
--- src/main/java/org/example/springsecuritybackend/domain/user/dto/request/LoginRequestDto.java
+++
@@ -1,4 +0,0 @@
-package org.example.springsecuritybackend.domain.user.dto.request;
-
-public record LoginRequestDto(String username, String password) {
-}
